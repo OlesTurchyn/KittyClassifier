886ac0a5471ae05041ec692119f2d5a6a0feb406
Oleksander Turchyn <52382133+OlesTurchyn@users.noreply.github.com>
2024-01-19 10:49:20 -0500

    WORKING MODEL AND CLOUD DEPLOYMENT!!

---
+++ gcp/main.py
@@ -0,0 +1,80 @@
+from google.cloud import storage
+import tensorflow as tf
+from PIL import Image
+import numpy as np
+
+model = None
+interpreter = None
+input_index = None
+output_index = None
+
+
+BUCKET_NAME = "turchyn-tf-models"
+class_names = [
+    "Abyssinian",
+    "American Bobtail",
+    "American Curl",
+    "American Shorthair",
+    "Bengal",
+    "Birman",
+    "Bombay",
+    "British Shorthair",
+    "Egyptian Mau",
+    "Exotic Shorthair",
+    "Maine Coon",
+    "Manx",
+    "Norwegian Forest",
+    "Persian",
+    "Ragdoll",
+    "Russian Blue",
+    "Scottish Fold",
+    "Siamese",
+    "Sphynx",
+    "Turkish Angora"
+]
+
+
+def download_blob(bucket_name, source_blob_name, destination_file_name):
+    """Downloads a blob from the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+
+    blob.download_to_filename(destination_file_name)
+
+    print(f"Blob {source_blob_name} downloaded to {destination_file_name}.")
+
+def predict(request):
+    global model
+    if model is None:
+        download_blob(
+            BUCKET_NAME,
+            "models/cats.h5",
+            "/tmp/cats.h5",
+        )
+        model = tf.keras.models.load_model("/tmp/cats.h5")
+
+    image = request.files["file"]
+
+    image = np.array(
+        Image.open(image).convert("RGB").resize((256, 256)) # image resizing
+    )
+
+    image = image.astype('uint8')
+
+    image = image/255 # normalize the image in 0 to 1 range
+
+    img_array = tf.expand_dims(image, 0)
+    predictions = model.predict(img_array)
+
+    print("Predictions:",predictions[0])
+    print("Prediction Length:", len(predictions[0]))
+
+    predicted_class = class_names[np.argmax(predictions[0])]
+    print("Predicted Class Index: ", np.argmax(predictions[0]))
+    print("Predicted Class Index: ", np.argmax(predictions[0]))
+    confidence = round(100 * (np.max(predictions[0])), 2)
+
+    return {"class": predicted_class, "confidence": confidence}
+ 
+ 
